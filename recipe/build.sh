8af4778881aa99865c8f5af189b4e9548e3c72ce
Ray Donnelly <nehaljw.kkd1@gmail.com>
2018-01-08 13:16:20 -0800

    Expect XQuartz in CONDA_BUILD_SYSROOT
    .. this means we cannot test it on macOS though.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
+
+if [[ ${HOST} =~ .*darwin.* ]]; then
+  if [[ ! -d ${CONDA_BUILD_SYSROOT}/opt/X11 ]]; then
+    echo "XQuartz not found in sysroot (${CONDA_BUILD_SYSROOT})"
+    echo "Please unpack it there via:"
+    echo "pushd /tmp"
+    echo "curl -SLO https://dl.bintray.com/xquartz/downloads/XQuartz-2.7.11.dmg"
+    echo "hdiutil attach XQuartz-2.7.11.dmg"
+    echo "pkgutil --expand /Volumes/XQuartz-2.7.11/XQuartz.pkg /tmp"
+    echo "pushd x11.pkg"
+    echo "tar -zxvf Payload"
+    echo "mv opt ${CONDA_BUILD_SYSROOT}"
+    echo "hdiutil detach /Volumes/XQuartz-2.7.11"
+    echo ".. or better, make conda packages for it"
+    exit 1
+  fi
+  X11_CONFIGURE_ARGS="--x-includes=${CONDA_BUILD_SYSROOT}/opt/X11/include --x-libraries=${CONDA_BUILD_SYSROOT}/opt/X11/lib"
+fi
+
 DISPLAY=:0 \
-  $R CMD INSTALL --build . --configure-args="--disable-ftgl"
+  $R CMD INSTALL --build . --configure-args="--disable-ftgl ${X11_CONFIGURE_ARGS}"
