6f04f1873e2f6413de1f1adb9086b5bef83804f7
dpryan79 <dpryan79@gmail.com>
2018-11-23 11:18:07 +0100

    We need mesa around, though I think this results in two X11 installations

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,7 @@ if [[ $target_platform =~ linux.* ]] || [[ $target_platform == win-32 ]] || [[ $
   export DISABLE_AUTOBREW=1
   mv DESCRIPTION DESCRIPTION.old
   grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
-  $R CMD INSTALL --build .
+  $R CMD INSTALL --build --configure-args="--x-includes=${PREFIX}/include --x-lib=${PREFIX}/lib" .
 else
   mkdir -p $PREFIX/lib/R/library/rgl
   mv * $PREFIX/lib/R/library/rgl
