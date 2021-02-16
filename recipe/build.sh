3e9745a7a1fb9dde0dccb0fb063ee1c53d7f04bc
Ista Zahn <izahn@hbs.edu>
2021-02-16 06:28:19 -0500

    Cleanup

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-X11_CONFIGURE_ARGS="--x-includes=${PREFIX}/include --x-libraries=${BUILD_PREFIX}/x86_64-conda_cos6-linux-gnu/sysroot/usr/lib64"
-export LD_LIBRARY_PATH=${BUILD_PREFIX}/x86_64-conda_cos6-linux-gnu/sysroot/usr/lib64
 
-DISPLAY=${DISPLAY:-:0} $R CMD INSTALL --build . --configure-args="--disable-ftgl ${X11_CONFIGURE_ARGS}"
+${R} CMD INSTALL --build . ${R_ARGS}
